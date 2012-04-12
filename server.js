3782f28dc1062012ff7269f7297f6f7edb23d8ba
k0lpak <kolpakovdenis21@gmail.com>
2012-04-12 23:50:23 +0300

    authentification implementation

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
-
 var mongo = require('mongoskin');
+
+
+
 var port = parseInt(process.argv.pop());
 
 var app = express.createServer();
@@ -9,37 +11,90 @@ var app = express.createServer();
 
 app.configure(function() {
     app.use(express.bodyParser());
+    app.use(express.cookieParser());
+	app.use(express.session({ secret: "keyboard cat" }));
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
+
+
 });
 
 app.set('view engine', 'jshtml');
 var mongoDb = mongo.db('localhost/trance_music');
 
 //Hardcoded credentials 
-function IsAuthentificated(user, password){
-	if (user == 'admin' && password == '12345'){
+function _isAllowAccess(user){
+	if (user.username == 'admin' && user.password == '12345'){
 		return true;
 	}
 	return false;
 }
 
-app.get('/', function(req, res) {
-	mongoDb.collection('articles').find().toArray(function(err, articles){
-		res.render( 'index',{
-			isAuthentificated : true,
-			user : {username : 'user'},
-			title : 'Trance Music',
+
+function _registerAuthCookie(user, req){
+	req.session.items = {credentials : user};
+}
+
+function _getCurrentUser(req){
+	return req.session.items && req.session.items.credentials ? req.session.items.credentials : undefined;
+}
+
+function _isAuthentificated(req){
+	return _getCurrentUser(req) ? true : false;
+}
+
+function _validatePermissions(req, res){
+	if (!_isAuthentificated(req)){
+		res.redirect('/login');
+	}
+}
+
+app.get('/login', function(req, res){
+	mongoDb.collection('articles').find().toArray(function(err,articles){
+		res.render('login', {
+			title:'Login',
 			articles : articles,
+			user : {username : '', password : ''},
 		});
 	});
 });
 
+app.post('/login', function(req, res){
+	if (_isAllowAccess(req.body)){
+		_registerAuthCookie(req.body, req);
+		res.redirect('/');
+	}else{
+		res.redirect('/login');
+	}
+});
+
+app.get('/logout', function(req, res){
+	if (req.session.items && req.session.items.credentials){
+		delete req.session.items.credentials;
+	}
+	res.redirect('/');
+});
+
+
+
+app.get('/', function(req, res) {
+	_validatePermissions(req, res);
+	mongoDb.collection('articles').find().toArray(function(err, articles){
+		res.render( 'index',{
+			isAuthentificated : _getCurrentUser(req),
+			user : _getCurrentUser(req),
+			title : 'Trance Music',
+			articles : articles,
+		});	
+	});
+});
+
 app.get('/article/add', function(req, res) {
+	_validatePermissions(req, res);
 	mongoDb.collection('articles').find().toArray(function(err, articles){
 		res.render( 'article_add',{
-			isAuthentificated : true,
-			user : {username : 'user'},
+			isAuthentificated : _getCurrentUser(req),
+			user : _getCurrentUser(req),
 			title : 'Add article',
 			articles : articles,
 			article : {headline : '', title : '', body :''},
@@ -49,6 +104,7 @@ app.get('/article/add', function(req, res) {
 });
 
 app.post('/article/add', function(req, res) {
+	_validatePermissions(req, res);
 	mongoDb.collection('articles').insert(req.body, function (err, result){
 		res.redirect('/');
 	});
@@ -56,10 +112,11 @@ app.post('/article/add', function(req, res) {
 });
 
 app.get('/cms', function(req, res){
+	_validatePermissions(req, res);
 	mongoDb.collection('articles').find().toArray(function(err, articles){
 		res.render( 'article_select',{
-			isAuthentificated : true,
-			user : {username:'user'},
+			isAuthentificated : _getCurrentUser(req),
+			user : _getCurrentUser(req),
 			title : 'CMS',
 			articles : articles,
 		});
@@ -67,6 +124,7 @@ app.get('/cms', function(req, res){
 });
 
 app.get('/article/delete/:id', function(req, res){
+	_validatePermissions(req, res);
 	var id = req.params.id;
 	mongoDb.collection('articles').remove({_id : mongoDb.bson_serializer.ObjectID.createFromHexString(id)}, function(err,result){
 		res.redirect('/cms/');
@@ -75,12 +133,12 @@ app.get('/article/delete/:id', function(req, res){
 
 app.get('/article/:id', function(req, res) {
 	var id = req.params.id;
-	
+	_validatePermissions(req, res);
 	mongoDb.collection('articles').findById(id, function(err, article){
 		mongoDb.collection('articles').find().toArray(function(err, articles){
 			res.render( 'article',{
-				isAuthentificated : true,
-				user : {username : 'user'},
+				isAuthentificated : _getCurrentUser(req),
+				user : _getCurrentUser(req),
 				title : article.title,
 				articles : articles,
 				article : article,
@@ -90,12 +148,13 @@ app.get('/article/:id', function(req, res) {
 });
 
 app.get('/article/edit/:id', function(req, res) {
+	_validatePermissions(req, res);
 	var id = req.params.id;
 	mongoDb.collection('articles').findById(id, function(err, article){
 		mongoDb.collection('articles').find().toArray(function(err, articles){
 			res.render( 'article_edit',{
-				isAuthentificated : true,
-				user : {username : 'user'},
+				isAuthentificated : _getCurrentUser(req),
+				user : _getCurrentUser(req),
 				title : 'Edit article',
 				articles : articles,
 				article : article,
@@ -105,6 +164,7 @@ app.get('/article/edit/:id', function(req, res) {
 });
 
 app.post('/article/edit/:id', function(req, res) {
+	_validatePermissions(req, res);
 	var id = req.params.id;
 	var article = req.body;
 	article._id = mongoDb.bson_serializer.ObjectID.createFromHexString(id);
